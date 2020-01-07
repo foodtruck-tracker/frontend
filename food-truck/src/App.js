d0b2af02875a765e6f922cdaea39dc19226374ee
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-01-06 17:26:14 -0800

    building login page, routing

--- food-truck/src/App.js
+++ food-truck/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import RegForm from "./components/RegForm";
+import Login from "./components/Login";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <RegForm />
-    </div>
+    <Router>
+      <div className="App">
+
+        <Route exact path="/" component={Login} />
+        <Route path= "/register" component={RegForm} />
+        
+      </div>
+    </Router>
   );
 }
 
